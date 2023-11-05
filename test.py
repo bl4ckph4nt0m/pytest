993f3c6b28e8c8b5ec429bb475c99e5fa298de46
bl4ckph4nt0m <136215756+bl4ckph4nt0m@users.noreply.github.com>
2023-11-05 09:44:20 -0500

    git clone rep

--- test.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Hello, World! with Streamlit")
-    st.write("This is a simple 'Hello, World!' program using Streamlit.")
-
-if __name__ == '__main__':
-    main()
