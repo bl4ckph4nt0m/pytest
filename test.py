69108ce7067611d40f501e0d23e65ac7baaa952b
bl4ckph4nt0m <136215756+bl4ckph4nt0m@users.noreply.github.com>
2023-11-05 09:29:28 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def main():
+    st.title("Hello, World! with Streamlit")
+    st.write("This is a simple 'Hello, World!' program using Streamlit.")
+
+if __name__ == '__main__':
+    main()
